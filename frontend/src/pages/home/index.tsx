fc1203efbe2c43227a071f326de370d1e2cdfffd
Vitor Bertti <berttivitor@gmail.com>
2020-07-21 18:20:54 -0300

    Added recent voting table

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
 import './styles.css';
 
+import { Link } from 'react-router-dom';
+
 const Home: React.FC = () => {
    return (
       <div className="Home">
          <h1 className="title">Welcome</h1>
+         <div className="recent-voting">
+            <table>
+               <thead>
+                  <tr>
+                     <th>Name</th>
+                     <th>Created by</th>
+                     <th>End date</th>
+                     <th>Active</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  <tr>
+                     <td>
+                        <Link to="/voting1">Voting 1</Link>
+                     </td>
+                     <td>
+                        <Link to="/user/profile">User</Link>
+                     </td>
+                     <td>2021/01/01</td>
+                     <td>true</td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <Link to="/voting2">Voting 2</Link>
+                     </td>
+                     <td>
+                        <Link to="/user/profile">User</Link>
+                     </td>
+                     <td>2021/01/01</td>
+                     <td>true</td>
+                  </tr>
+               </tbody>
+            </table>
+         </div>
       </div>
    );
 };
